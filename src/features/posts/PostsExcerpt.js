d1a5f5f1e7fc7a0406d8f127348bbcf9f68a2c16
Tobi Olasimbo <tobiolasimbo84@gmail.com>
2022-09-05 07:07:53 +0100

    updated css

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -13,7 +13,7 @@ const PostsExcerpt = ({ postId }) => {
             <h2>{post.title}</h2>
             <p className="excerpt">{post.body.substring(0, 75)}...</p>
             <p className="postCredit">
-                <Link to={`post/${post.id}`}>View Post</Link>
+                <Link to={`post/${post.id}`} className="viewPost">View Post</Link>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
